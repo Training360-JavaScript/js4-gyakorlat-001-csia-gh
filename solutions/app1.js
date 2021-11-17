5693c9ef5120bd8b6a2038355e505ec5acd8c1ac
Cserhati Imre Andras <cserhati.imre.andras@gmail.com>
2021-11-17 17:18:12 +0100

    ready to push

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,14 @@
+const converterPromise = (arr = []) => {
+  return new Promise((resolve, reject) => {
+    const upperCaseArr = arr.map((el) => {
+      if (typeof el !== 'string') {
+        reject('Error: Not all elements are string type!');
+      } else {
+        return el.toLocaleUpperCase();
+      }
+    });
+    resolve(upperCaseArr);
+  });
+};
 
-
-
-
-
-
-// export default converterPromise;
+export default converterPromise;
